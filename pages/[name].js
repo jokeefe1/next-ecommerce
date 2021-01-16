29fa603d1b311c7aa355ae41fc1f67608538b395
John O'Keefe <jokeefe@weegoglobal.com>
2021-01-15 23:48:57 -0500

    initial commit

---
+++ pages/[name].js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {useRouter} from 'next/router'
+
+export default function Name() {
+    const router = useRouter()
+    const {name} = router.query
+
+    return (
+        <h1>Welcome {name}</h1>
+    )
+}
---
+++ pages/_app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <nav>
+        <a href='/'>home</a>
+        <a href='/about'>about</a>
+      </nav>
+      <Component {...pageProps} />
+      <footer>This is my fancy footer</footer>
+    </>
+  );
+}
